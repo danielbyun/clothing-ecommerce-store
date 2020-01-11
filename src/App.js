4a92fe93636d773653d9ffa37e460e477fef07c4
Daniel Byun <dbyun@doinglab.com>
2020-01-11 21:28:22 +0900

    updating our app with a shop page, collection preview component and collection item component. (section 6 video 3)

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React, { Fragment } from "react";
 import "./App.css";
 import Homepage from "./Pages/Homepage/Homepage";
 import { Route, Switch } from "react-router-dom";
-
-const HatsPage = () => (
-  <div>
-    <h1>hats page son</h1>
-  </div>
-);
+import ShopPage from "./Pages/Shop/Shop";
 
 const App = () => {
   return (
     <Fragment>
       <Switch>
-        <Homepage exact path="/" />
-        <Route exact path="/shop/hats" component={HatsPage} />
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </Fragment>
   );
---
+++ src/Components/Preview-Collection/Collection.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Collection.scss";
+import CollectionItem from "../Collection-Item/CollectionItem";
+
+const CollectionPreview = ({ title, items }) => {
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {/* performance concern whenever this component has to re-render */}
+        {items
+          // only show 4 items
+          .filter((item, idx) => idx < 4)
+          .map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default CollectionPreview;
